e12f807327e4e2bf6d654472005eb09f57439dd1
nadaa <nadaa@ajou.ac.kr>
2026-08-03 23:10:34 +0900

    feat: UserRequestDto 구현

---
+++ user-service/src/main/java/com/logistics/user/dto/request/UserRequestDto.java
@@ -0,0 +1,39 @@
+package com.logistics.user.dto.request;
+
+import com.logistics.user.core.UserRole;
+import com.logistics.user.core.UserStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+public class UserRequestDto {
+
+    @NotBlank(message = "아이디는 필수입니다.")
+    @Pattern(
+        regexp = "^[a-z0-9]+$",
+        message = "아이디는 4~10자의 소문자(a~z)와 숫자(0~9)로만 구성되어야 합니다."
+    )
+    @Size(min = 4, max = 10)
+    String username;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    @Pattern(// 대문자 + 소문자 + 숫자 + 특수문자 각 1개 이상, 공백 불가, 8~15자
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&])(?=\\S+$).{8,15}$",
+        message = "비밀번호는 8~15자이며 대문자·소문자·숫자·특수문자(@$!%*#?&)를 각각 1개 이상 포함해야 합니다."
+    )
+    @Size(min = 8, max = 15)
+    String password;
+
+
+    String slack_id;
+    UserRole role;
+    UUID hubId;
+    UUID companyId;
+}
