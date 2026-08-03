fead6688f443cafff2c8741d91dd84e8e7b33c50
nadaa <nadaa@ajou.ac.kr>
2026-08-03 23:09:40 +0900

    feat: UserRepository 구현

---
+++ user-service/src/main/java/com/logistics/user/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.logistics.user.repository;
+
+import com.logistics.user.core.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID>{
+    boolean existsBySlackId(String slackId);
+
+    boolean existsByUsername(String username);
+}
