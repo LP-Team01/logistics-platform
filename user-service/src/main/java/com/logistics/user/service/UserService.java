aa4f0ae2a3a4d6891ab1e43b6869e892c10ba04e
nadaa <nadaa@ajou.ac.kr>
2026-08-03 23:11:59 +0900

    feat: Auth & User Service 생성

---
+++ user-service/src/main/java/com/logistics/user/service/UserService.java
@@ -0,0 +1,16 @@
+package com.logistics.user.service;
+
+import com.logistics.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+}
