b607d450baa3ff910be82853aa554050d37931af
nadaa <nadaa@ajou.ac.kr>
2026-08-04 11:47:35 +0900

    refactor: user-service DTO를 record로 전환

--- user-service/src/main/java/com/logistics/user/dto/response/UserResponseDto.java
+++ user-service/src/main/java/com/logistics/user/dto/response/UserResponseDto.java
@@ -3,31 +3,26 @@ package com.logistics.user.dto.response;
 import com.logistics.user.core.User;
 import com.logistics.user.core.UserRole;
 import com.logistics.user.core.UserStatus;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
 
 import java.util.UUID;
 
-@Getter
-@Builder
-public class UserResponseDto {
-    private UUID userId;
-    private String username;
-    private UserRole role;
-    private UserStatus status;
-    private UUID hubId;
-    private UUID companyId;
+public record UserResponseDto(
+    UUID userId,
+    String username,
+    UserRole role,
+    UserStatus status,
+    UUID hubId,
+    UUID companyId
+) {
 
-
-    public static UserResponseDto from(User user){
-        return UserResponseDto.builder()
-            .userId(user.getUserId())
-            .username(user.getUsername())
-            .role(user.getRole())
-            .status(user.getStatus())
-            .hubId(user.getHubId())
-            .companyId(user.getCompanyId())
-            .build();
+    public static UserResponseDto from(User user) {
+        return new UserResponseDto(
+            user.getUserId(),
+            user.getUsername(),
+            user.getRole(),
+            user.getStatus(),
+            user.getHubId(),
+            user.getCompanyId()
+        );
     }
 }
