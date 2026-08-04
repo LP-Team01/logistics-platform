7e12407d4d6fea7bc68d72145f87438000aea60d
nadaa <nadaa@ajou.ac.kr>
2026-08-04 11:51:45 +0900

    chore: import 와일드카드 제거

--- user-service/src/main/java/com/logistics/user/controller/UserController.java
+++ user-service/src/main/java/com/logistics/user/controller/UserController.java
@@ -1,15 +1,9 @@
 package com.logistics.user.controller;
 
-import com.logistics.user.dto.request.UserRequestDto;
-import com.logistics.user.dto.response.UserResponseDto;
 import com.logistics.user.service.UserService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
