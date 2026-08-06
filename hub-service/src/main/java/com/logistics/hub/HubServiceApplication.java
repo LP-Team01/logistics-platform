96937a3117f532b8be8199951816f0123ecd6d49
poppq03 <poppq03@naver.com>
2026-08-06 11:20:21 +0900

    fix: EnableJpaAuditing을 별도 Config로 분리

--- hub-service/src/main/java/com/logistics/hub/HubServiceApplication.java
+++ hub-service/src/main/java/com/logistics/hub/HubServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableCaching
 public class HubServiceApplication {
     public static void main(String[] args) {
