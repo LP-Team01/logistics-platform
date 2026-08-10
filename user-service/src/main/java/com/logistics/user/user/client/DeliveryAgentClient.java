a7c56b0365a3be2df2718a8813d5d723187698be
nadaa <nadaa@ajou.ac.kr>
2026-08-10 11:55:07 +0900

    Feat: DeliveryAgentClient 구현

---
+++ user-service/src/main/java/com/logistics/user/user/client/DeliveryAgentClient.java
@@ -0,0 +1,18 @@
+package com.logistics.user.user.client;
+
+import java.util.UUID;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "delivery-service")
+public interface DeliveryAgentClient {
+
+    @PostMapping("/api/delivery-agents")
+    DeliveryAgentResponse createAgent(
+        @RequestHeader("X-User-Role") String userRole,
+        @RequestHeader(value = "X-Hub-Id", required = false) UUID requesterHubId,
+        @RequestBody CreateDeliveryAgentRequest request
+    );
+}
